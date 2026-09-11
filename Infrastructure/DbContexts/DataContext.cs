8781ed80863e5a542acb7e212b1051d45fc25532
annashemchuk00 <annashemchuk00@gmail.com>
2026-09-11 13:56:12 +0300

    Connected the database and ran the first migration

---
+++ Infrastructure/DbContexts/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TechnicalTasks.Domain.Entities;
+
+namespace TechnicalTask.Infrastructure.DbContexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ConferenceRoom> ConferenceRooms { get; set; }
+
+        public DbSet<Booking> Bookings { get; set; }
+
+        public DbSet<Service> Services { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //налаштування зв'язку між ConferenceRoom та Booking
+            modelBuilder.Entity<ConferenceRoom>()
+                .HasMany(r => r.Bookings)
+                .WithOne(b => b.ConferenceRoom)
+                .HasForeignKey(b => b.RoomId);
+
+            //налаштування зв'язку між Service та Booking
+            modelBuilder.Entity<Booking>()
+                .HasMany(b => b.SelectedServices)
+                .WithMany(s => s.Bookings);
+
+            //налаштування зв'язку між Service та ConferenceRoom
+            modelBuilder.Entity<ConferenceRoom>()
+                .HasMany(r => r.Services)
+                .WithMany(s => s.ConferenceRooms);
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20260911103326_InitialCreate.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechnicalTasks.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ConferenceRooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    BasePricePerHour = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConferenceRooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_ConferenceRooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "ConferenceRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConferenceRoomService",
+                columns: table => new
+                {
+                    ConferenceRoomsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ServicesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConferenceRoomService", x => new { x.ConferenceRoomsId, x.ServicesId });
+                    table.ForeignKey(
+                        name: "FK_ConferenceRoomService_ConferenceRooms_ConferenceRoomsId",
+                        column: x => x.ConferenceRoomsId,
+                        principalTable: "ConferenceRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConferenceRoomService_Services_ServicesId",
+                        column: x => x.ServicesId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookingService",
+                columns: table => new
+                {
+                    BookingsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SelectedServicesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingService", x => new { x.BookingsId, x.SelectedServicesId });
+                    table.ForeignKey(
+                        name: "FK_BookingService_Bookings_BookingsId",
+                        column: x => x.BookingsId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookingService_Services_SelectedServicesId",
+                        column: x => x.SelectedServicesId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingService_SelectedServicesId",
+                table: "BookingService",
+                column: "SelectedServicesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConferenceRoomService_ServicesId",
+                table: "ConferenceRoomService",
+                column: "ServicesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookingService");
+
+            migrationBuilder.DropTable(
+                name: "ConferenceRoomService");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "ConferenceRooms");
+        }
+    }
+}
